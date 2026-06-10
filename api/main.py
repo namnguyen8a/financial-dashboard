a66158d353b9df06ec769894acd395da88a00f47
namnguyen <namnguyen.8a@gmail.com>
2026-06-10 19:34:45 +0700

    feat: add uvicorn run in api/main file

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,3 +11,5 @@ def read_root():
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
